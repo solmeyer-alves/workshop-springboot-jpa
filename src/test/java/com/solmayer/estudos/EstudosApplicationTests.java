c317d2aca170302c33705cbfab5e26d8f1374f6a
solmayer <solmayer.alves@gmail.com>
2025-12-23 20:09:18 -0300

    Project Created

---
+++ src/test/java/com/solmayer/estudos/EstudosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solmayer.estudos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
