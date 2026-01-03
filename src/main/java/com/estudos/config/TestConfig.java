046d233c2d46b178d91b47b48fdb17898865895c
solmayer <solmayer.alves@gmail.com>
2026-01-03 10:15:34 -0300

    Payment, one-to-one association

--- src/main/java/com/estudos/config/TestConfig.java
+++ src/main/java/com/estudos/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.estudos.entities.Category;
 import com.estudos.entities.Order;
 import com.estudos.entities.OrderItem;
+import com.estudos.entities.Payment;
 import com.estudos.entities.Product;
 import com.estudos.entities.User;
 import com.estudos.enums.OrderStatus;
@@ -47,8 +48,8 @@ public class TestConfig implements CommandLineRunner {
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.WAITING_PAYMENT, u1); 
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.PAID, u2); 
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1); 
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2); 
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.SHIPPED, u1);
 		
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
@@ -82,5 +83,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T20:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
