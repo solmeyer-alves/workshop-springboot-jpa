d7dbe1f7baa1e04bed8468cf418b5d210d834422
solmayer <solmayer.alves@gmail.com>
2026-01-04 11:14:41 -0300

    User delete

--- src/main/java/com/estudos/controller/UserController.java
+++ src/main/java/com/estudos/controller/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,12 +37,14 @@ public class UserController {
 		
 	}
 	
-	/* @PostMapping
-	public ResponseEntity<User> insert(@RequestBody User obj){
-		obj = service.insertUser(obj);
-		return ResponseEntity.status(HttpStatus.CREATED).body(obj);
-	}*/
+	//Método que usaria informando o status 201 CREATED.
+	//@PostMapping 
+	//public ResponseEntity<User> insert(@RequestBody User obj){
+	//	obj = service.insertUser(obj);
+	//	return ResponseEntity.status(HttpStatus.CREATED).body(obj);
+	//}
 	
+	//Método utilizado no curso informando o status 201 CREATED e o location com id do usuário criado.
 	@PostMapping
 	public ResponseEntity<User> insert(@RequestBody User obj){
 		obj = service.insertUser(obj);
@@ -50,5 +53,11 @@ public class UserController {
 				.path("/{id}")
 				.buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).body(obj);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		service.deleteById(id);
+		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/com/estudos/services/UserService.java
+++ src/main/java/com/estudos/services/UserService.java
@@ -27,4 +27,8 @@ public class UserService {
 	public User insertUser(User obj) {
 		return repository.save(obj);
 	}
+	
+	public void deleteById(Long id) {
+		repository.deleteById(id);
+	}
 }
