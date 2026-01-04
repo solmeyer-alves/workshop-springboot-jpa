1e001cc51006a0844251992c988d07bd5885d2a1
solmayer <solmayer.alves@gmail.com>
2026-01-04 10:54:07 -0300

    User insert

--- src/main/java/com/estudos/controller/UserController.java
+++ src/main/java/com/estudos/controller/UserController.java
@@ -1,13 +1,17 @@
 package com.estudos.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.estudos.entities.User;
 import com.estudos.services.UserService;
@@ -32,4 +36,19 @@ public class UserController {
 		
 	}
 	
+	/* @PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj){
+		obj = service.insertUser(obj);
+		return ResponseEntity.status(HttpStatus.CREATED).body(obj);
+	}*/
+	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj){
+		obj = service.insertUser(obj);
+		URI uri = ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
 }
