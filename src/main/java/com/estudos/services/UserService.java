cab2ef0287f4ad328a096cf078048887bc3d0b16
solmayer <solmayer.alves@gmail.com>
2026-01-07 15:18:35 -0300

    Exception handling - update

--- src/main/java/com/estudos/services/UserService.java
+++ src/main/java/com/estudos/services/UserService.java
@@ -5,13 +5,14 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.estudos.entities.User;
 import com.estudos.repositories.UserRepository;
 import com.estudos.services.exceptions.DatabaseException;
 import com.estudos.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class UserService {
@@ -45,9 +46,14 @@ public class UserService {
 	}
 
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
 		return repository.save(entity);
+		}
+		catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}	
 	}
 
 	private void updateData(User entity, User obj) {
